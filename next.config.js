d991a5dc2d81a694ceb2652eb36a4cbbc3684094
MatthieuNimal <nimal160604@gmail.com>
2025-11-07 15:37:47 +0100

    Updated .boltignore

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
     domains: ['res.cloudinary.com', 'upload.wikimedia.org'],
   },
   reactStrictMode: true,
+  // Optimizations for Bolt Cloud deployment
+  productionBrowserSourceMaps: false, // Disable source maps in production
+  compress: true, // Enable gzip compression
+  poweredByHeader: false, // Remove X-Powered-By header
+  generateEtags: false, // Disable ETag generation
+  swcMinify: true, // Use SWC for minification (faster)
 };
 
 module.exports = nextConfig;
